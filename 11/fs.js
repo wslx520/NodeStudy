dfd8397d4594ba2634667603d562da88b02c5f3b
XLee <lix9nw@gmail.com>
2017-07-02 22:28:08 +0800

    11 fs

--- 11/fs.js
+++ 11/fs.js
@@ -11,11 +11,13 @@ let fs = require('fs');
  */
 
 // 同步打开文件 openSync ,返回一个文件描述符
-
-
 let fd = fs.openSync('./2.txt', 'r');
 let buf = new Buffer(3);
-
+/**
+ * 文件描述符起始为3
+ * 因为 0: stdin, 1: stdout, 2: stderr
+ */
+fs.write(2, '其实是调用了stderr', err => {});
 /**
  * readSync 
  * 
@@ -33,3 +35,30 @@ fs.readSync(fd, buf, 0, 3, 0);
 // })
 console.log(fd);
 console.log(buf.toString());
+
+function readToEnd(file, callback) {
+    let list = [];
+    let len = 3;
+    fs.open(file, 'r', function (err, fd) {
+        let pos = 0;
+        function read() {
+            let buff = new Buffer(len);
+            fs.read(fd, buff, 0, len, pos, function (err, bytes) {
+                // bytes 读取到的字节数
+                list.push(buff);
+                pos += bytes;
+                if (bytes > 0) {
+                    read();
+                } else {
+                    let result = Buffer.concat(list);
+                    callback(result.slice(0, pos));
+                }
+            });
+        }
+        read();
+    })
+}
+
+readToEnd('2.txt', function (res) {
+    console.log(res.toString());
+});
